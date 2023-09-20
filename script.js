80ca0ca05d6d4d73e2cb8b0cb936d86075268ae9
Ajay S <ajaycshan@gmail.com>
2023-09-20 23:39:24 +1000

    Amend from jQuery to JS for colourCode logic

--- script.js
+++ script.js
@@ -8,15 +8,32 @@ var today = dayjs()
 var currentDate = today.format('dddd D MMMM, YYYY')
 var currentHour = today.format('H')
 
-console.log(typeof textareaEls[8].dataset.hour)
 // Display current day at top of calendar
 currentDayEl.text(currentDate);
 
-
 // TODO: Colour-code past, present, future
-// function colourCode() {
+for ( var i = 0; i < textareaEls.length; i++) {
+    var indivTextareaEl = textareaEls[i];
+    colourCode();
+}
 
-// }
+function colourCode() {
+    // Ensures consistent datatypes
+    var calHour = parseInt(indivTextareaEl.dataset.hour);
+    console.log(calHour)
+    if (currentHour > calHour) {
+        // Adds 'past' class to textarea element for specific hour
+        // indivTextareaEl.addClass('past');
+        indivTextareaEl.classList.add('past')
+    } else if (currentHour === calHour) {
+        // Adds 'present class to textarea element for specific hour
+        // indivTextareaEl.addClass('present');
+        indivTextareaEl.classList.add('present')
+    } else if (currentHour < calHour) {
+        // indivTextareaEl.addClass('future');
+        indivTextareaEl.classList.add('future')
+    };
+}
 
 // TODO: Saving events to local storage
 // Listener for click events on save button
