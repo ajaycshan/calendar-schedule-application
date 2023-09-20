1c1e903e136b052a65e1cbdc890d2d7df32dc836
Ajay S <ajaycshan@gmail.com>
2023-09-20 23:01:59 +1000

    Add hour data attributes to HTML

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 
 // TODO: Declaring variables
 var currentDayEl = $('#currentDay')
+var textareaEls = $('textarea')
+
 
 var today = dayjs()
 var currentDate = today.format('dddd D MMMM, YYYY')
 var currentHour = today.format('H')
 
+console.log(typeof textareaEls[8].dataset.hour)
 // Display current day at top of calendar
 currentDayEl.text(currentDate);
 
+
 // TODO: Colour-code past, present, future
+// function colourCode() {
+
+// }
 
 // TODO: Saving events to local storage
 // Listener for click events on save button
